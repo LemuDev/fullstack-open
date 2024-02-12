1bf40828c948483ebbb4c970cb77c50611842a4f
unknown <cruzlemuel5@gmail.com>
2024-02-12 10:49:45 -0400

    init Form

--- part2/src/components/FormPersons.tsx
+++ part2/src/components/FormPersons.tsx
@@ -1,17 +1,50 @@
+import React, { useState } from "react"
+
+type dataForm = {
+  name: string,
+  number: string
+}
+
+
 
 export function FormPersons() {
+  const [values, setValues] = useState<dataForm>(
+    {
+      name: '',
+      number: ''
+    }
+  )
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target)
+
+    setValues(
+       {
+        ...values,
+        [e.currentTarget.name] : e.currentTarget.value
+      }
+    )
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    
+
+    console.log(values)
+  }
+
   return (
-    <form className="container p-3 bg-light rounded  border" style={{maxWidth:"600px"}}>
+    <form className="container p-3 bg-light rounded  border" style={{maxWidth:"600px"}} onSubmit={handleSubmit} >
         <h5 className="display-4 text-center">Create Person</h5>
 
         <div className="form-group my-2">
             <label className="mx-1" htmlFor="name">Name</label>
-            <input type="text" id="name"placeholder="Full Name..." className="form-control" />
+            <input type="text" id="name"placeholder="Full Name..." className="form-control" value={values.name} onChange={handleChange} name="name"/>
         </div>
 
         <div className="form-group my-2">
             <label className="mx-1" htmlFor="number">Number</label>
-            <input type="text" id="number"placeholder="Number..." className="form-control" />
+            <input type="text" id="number"placeholder="Number..." className="form-control" value={values.number} onChange={handleChange} name="number"/>
         </div>
 
         <input type="submit" className="btn btn-primary w-100"/>
