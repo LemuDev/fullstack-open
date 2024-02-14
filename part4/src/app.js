4257ec36d143eef7649f564f6f06b7cd7ccd8e62
unknown <cruzlemuel5@gmail.com>
2024-02-14 13:52:20 -0400

    init request middlewares

--- part4/src/app.js
+++ part4/src/app.js
@@ -1,11 +1,21 @@
 const express = require("express")
+const req_middleware = require("./utils/middlewares/request")
+
 const app = express()
 
 const notesRoute = require("./controllers/note")
 
+
+// Routers
 app.use("/api/notes", notesRoute)
 
 
+
+
+// Middlewares
+app.use(req_middleware.errorHandler)
+app.use(req_middleware.requestLogger)
+app.use(req_middleware.unknownEndpoint)
 
 
 module.exports = app
